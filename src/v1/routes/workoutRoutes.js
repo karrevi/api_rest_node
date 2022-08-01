8a6c0b6eb37a3df0c5b4ccbf510c9b5cb4818f22
Carlos Revilla <clro26.k@gmail.com>
2022-08-01 22:34:57 +0200

    refactor routes and create endpoints

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,24 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const workoutController = require("../../controllers/workoutController")
 
-router.get('/', (req, res) => {
-        res.send("Get all workouts")
-    })
-
-    .get("/:workoutId", (req, res) => {
-        res.send(`Get workout ${req.params.workoutId}`)
-    })
-
-    .post("/:workoutId", (req, res) => {
-        res.send(`Create workout ${req.params.workoutId}`)
-    })
-
-    .patch("/:workoutId", (req, res) => {
-        res.send(`Update workout ${req.params.workoutId}`)
-    })
-
-    .delete("/:workoutId", (req, res) => {
-        res.send(`Delete workout ${req.params.workoutId}`)
-    })
+router
+    .get("/", workoutController.getAllWorkouts)
+    .get("/:workoutId", workoutController.getOneWorkout)
+    .post("/:workoutId", workoutController.createNewWorkout)
+    .patch("/:workoutId", workoutController.updateOneWorkout)
+    .delete("/:workoutId", workoutController.deleteOneWorkout)
 
 module.exports = router;
