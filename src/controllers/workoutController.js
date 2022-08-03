6576f589673870a89f03c82ab0c4e885ddde2acd
Carlos Revilla <clro26.k@gmail.com>
2022-08-03 17:05:16 +0200

    Implementation services. Data model. Middlewares

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,20 +1,30 @@
+const workoutService = require('../services/workoutService')
+
 const getAllWorkouts = (req, res) => {
-    res.send("Get all workouts");
+    const allWorkouts = workoutService.getAllWorkouts()
+    res.send({
+        status: 'OK',
+        data: allWorkouts
+    });
 }
 
 const getOneWorkout = (req, res) => {
+    const workout = workoutService.getOneWorkout(req.params.workoutId)
     res.send(`Get workout ${req.params.workoutId}`);
 }
 
 const createNewWorkout = (req, res) => {
+    const createWorkout = workoutService.createNewWorkout(req.params.workoutId)
     res.send(`Create workout ${req.params.workoutId}`)
 }
 
 const updateOneWorkout = (req, res) => {
+    const updateworkout = workoutService.updateOneWorkout(req.params.workoutId)
     res.send(`Update workout ${req.params.workoutId}`)
 }
 
 const deleteOneWorkout = (req, res) => {
+    workoutService.deleteOneWorkout(req.params.workoutId)
     res.send(`Delete workout ${req.params.workoutId}`)
 }
 
